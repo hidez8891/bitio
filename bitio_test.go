a7d89b4e66113e037baa17aaf3d760f60d38951b
hidez8891 <minowith+github@gmail.com>
2016-09-17 14:14:06 +0900

    testcase cool update

--- bitio_test.go
+++ bitio_test.go
@@ -2,6 +2,8 @@ package bitio
 
 import (
 	"bytes"
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -11,20 +13,51 @@ type TestStruct1 struct {
 	Val3 int `byte:"3"`
 }
 
-func TestReader(t *testing.T) {
-	data := []byte{0x0a, 0xff, 0x1c, 0xff, 0x01, 0x1c}
-	vals := TestStruct1{}
-	if err := Read(&vals, bytes.NewReader(data)); err != nil {
-		t.Fatal("Read error:", err)
-	}
+type readTester struct {
+	src []byte
+	dst interface{}
+	ans map[string]interface{}
+}
 
-	if vals.Val1 != 0x0a {
-		t.Fatalf("Read got %x, want %x ", vals.Val1, 0x0a)
+var readtests = []readTester{
+	{
+		src: []byte{0x0a, 0xff, 0x1c, 0xff, 0x01, 0x1c},
+		dst: &TestStruct1{},
+		ans: map[string]interface{}{
+			"Val1": 0x0a,
+			"Val2": 0x1cff,
+			"Val3": 0x1c01ff,
+		},
+	},
+}
+
+func TestReader(t *testing.T) {
+	for _, test := range readtests {
+		dst := test.dst
+		if err := Read(dst, bytes.NewReader(test.src)); err != nil {
+			t.Fatal("Read error:", err)
+		}
+
+		rv := reflect.ValueOf(dst).Elem()
+		rt := rv.Type()
+
+		for i := 0; i < rv.NumField(); i++ {
+			v := rv.Field(i)
+			f := rt.Field(i)
+			if f.PkgPath != "" {
+				continue
+			}
+
+			name := f.Name
+			if tostrCompare(v, test.ans[name]) == false {
+				t.Fatalf("%s read %v, want %v", name, v, test.ans[name])
+			}
+		}
 	}
-	if vals.Val2 != 0x1cff {
-		t.Fatalf("Read got %x, want %x ", vals.Val1, 0x1cff)
-	}
-	if vals.Val3 != 0x1c01ff {
-		t.Fatalf("Read got %x, want %x ", vals.Val1, 0x1c01ff)
-	}
+}
+
+func tostrCompare(a, b interface{}) bool {
+	as := fmt.Sprintf("%v", a)
+	bs := fmt.Sprintf("%v", b)
+	return as == bs
 }
