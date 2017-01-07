e8a99ca4e4d96cc6b125148fe19e81b48cc962b8
hidez8891 <minowith+github@gmail.com>
2017-01-07 23:28:57 +0900

    add BitWriter concrete class

--- bitbuffer.go
+++ bitbuffer.go
@@ -9,14 +9,15 @@ import (
 type BitReader interface {
 	ReadBit(p *byte, bitSize int) (nBit int, err error)
 	ReadBits(p []byte, bitSize int) (nBit int, err error)
-	Read(p []byte) (nByte int, err error)
+	Read(p []byte) (nBit int, err error)
 }
 
 // BitWriter is the interface bit/byte writting method
 type BitWriter interface {
 	WriteBit(p byte, bitSize int) (nBit int, err error)
 	WriteBits(p []byte, bitSize int) (nBit int, err error)
-	Write(p []byte) (nByte int, err error)
+	Write(p []byte) (nBit int, err error)
+	Flush() error
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -107,7 +108,7 @@ func (obj *BitReadBuffer) ReadBits(p []byte, bitSize int) (nBit int, err error) 
 	return
 }
 
-// ReadBit reads data len(p) size and returns read size.
+// Read reads data len(p) size and returns read size.
 // If error happen, err will be set.
 func (obj *BitReadBuffer) Read(p []byte) (nBit int, err error) {
 	if err = obj.tryRead(); err != nil {
@@ -169,5 +170,133 @@ func (obj *BitReadBuffer) forceRead() error {
 
 	obj.buff = b[0]
 	obj.left = 8
+	return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// NewBitWriteBuffer returns BitWriteBuffer
+func NewBitWriteBuffer(w io.Writer) *BitWriteBuffer {
+	return &BitWriteBuffer{
+		w:    w,
+		buff: 0,
+		left: 0,
+	}
+}
+
+// BitWriteBuffer is implemented by BitWriteer
+type BitWriteBuffer struct {
+	w    io.Writer
+	buff byte
+	left int
+}
+
+// WriteBit writes single data (bitSize) and returns write size.
+// If error happen, err will be set.
+// Write data is used right justified. (4bit = 0x0f)
+func (obj *BitWriteBuffer) WriteBit(p byte, bitSize int) (nBit int, err error) {
+	p <<= uint(8 - bitSize)
+
+	if obj.left+bitSize > 8 {
+		n := 8 - obj.left
+		obj.buff <<= uint(n)
+		obj.buff |= p >> uint(8-n)
+		obj.left += n
+
+		if err = obj.forceWrite(); err != nil {
+			return
+		}
+		nBit += n
+
+		p <<= uint(n)
+		bitSize -= n
+	}
+
+	obj.buff <<= uint(bitSize)
+	obj.buff |= p >> uint(8-bitSize)
+	obj.left += bitSize
+
+	if err = obj.tryWrite(); err != nil {
+		return
+	}
+	nBit += bitSize
+
+	return
+}
+
+// WriteBits writes data (bitSize) and returns write size.
+// If error happen, err will be set.
+// Write data is used right justified. (12bit = 0x0f 0xff)
+func (obj *BitWriteBuffer) WriteBits(p []byte, bitSize int) (nBit int, err error) {
+	if len(p)*8 < bitSize {
+		return 0, fmt.Errorf("bitio: argument p[] is %d bits, want %d bits", len(p)*8, bitSize)
+	}
+
+	allBitSize := bitSize + obj.left
+	bitByte := (bitSize + 7) / 8
+	allByte := (allBitSize + 7) / 8
+	pp := make([]byte, allByte)
+	copy(pp[:bitByte], p[:bitByte])
+
+	if bitSize%8 > 0 {
+		leftShift(pp, uint(8-bitSize%8))
+	}
+	rightShift(pp, uint(obj.left))
+	pp[0] |= obj.buff << uint(8-obj.left)
+	obj.buff = 0
+	obj.left = 0
+
+	if allBitSize%8 > 0 {
+		obj.left = allBitSize % 8
+		obj.buff = pp[len(pp)-1] >> uint(8-obj.left)
+		pp = pp[:len(pp)-1]
+	}
+
+	if _, err = obj.w.Write(pp); err != nil {
+		return
+	}
+	nBit = bitSize
+
+	return
+}
+
+// Write writes data len(p) size and returns write size.
+// If error happen, err will be set.
+func (obj *BitWriteBuffer) Write(p []byte) (nBit int, err error) {
+	return obj.WriteBits(p, len(p)*8)
+}
+
+// Flush writes data if obj.buff is not empty.
+// If error happen, err will be set.
+func (obj *BitWriteBuffer) Flush() error {
+	return obj.forceWrite()
+}
+
+// tryWrite writes 1 byte data if obj.buff is full.
+// If error happen, returns err.
+func (obj *BitWriteBuffer) tryWrite() error {
+	if obj.left == 8 {
+		return obj.forceWrite()
+	}
+	return nil
+}
+
+// forceWrite writes 1 byte data. (0 right padding)
+// If error happen, returns err.
+func (obj *BitWriteBuffer) forceWrite() error {
+	if obj.left == 0 {
+		return nil
+	}
+
+	obj.buff <<= uint(8 - obj.left)
+	b := make([]byte, 1)
+	b[0] = obj.buff
+
+	if _, err := obj.w.Write(b); err != nil {
+		return err
+	}
+
+	obj.buff = 0
+	obj.left = 0
 	return nil
 }
--- bitbuffer_test.go
+++ bitbuffer_test.go
@@ -103,6 +103,7 @@ func TestBitReadBuffer_ReadBits(t *testing.T) {
 		}
 	}
 }
+
 func TestBitReadBuffer_ReadBits_Loop(t *testing.T) {
 	str := "" +
 		"1" +
@@ -232,6 +233,254 @@ func TestBitReadBuffer_Read_Combination(t *testing.T) {
 		}
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+func TestBitWriteBuffer_WriteBit(t *testing.T) {
+	var tests = []struct {
+		data byte
+		bits int
+		exp  []byte
+	}{
+		{0xff, 1, []byte{0x80}},
+		{0xff, 7, []byte{0xfe}},
+		{0xff, 8, []byte{0xff}},
+	}
+
+	for _, tt := range tests {
+		var n int
+		var err error
+
+		b := bytes.NewBuffer([]byte{})
+		w := NewBitWriteBuffer(b)
+		if n, err = w.WriteBit(tt.data, tt.bits); err != nil {
+			t.Fatalf("WriteBit happen error %v", err)
+		}
+		if err = w.Flush(); err != nil {
+			t.Fatalf("WriteBit happen error %v", err)
+		}
+
+		if n != tt.bits {
+			t.Fatalf("WriteBit write size %d, want %d", n, tt.bits)
+		}
+
+		if reflect.DeepEqual(b.Bytes(), tt.exp) == false {
+			t.Fatalf("WriteBit write data %x, want %x", b.Bytes(), tt.exp)
+		}
+	}
+}
+
+func TestBitWriteBuffer_WriteBit_Loop(t *testing.T) {
+	str := "" +
+		"1" +
+		"10" +
+		"011" +
+		"0100" +
+		"00101"
+	exp := binaryToByteArray(str)
+
+	b := bytes.NewBuffer([]byte{})
+	w := NewBitWriteBuffer(b)
+	for i := 1; i <= 5; i++ {
+		var n int
+		var err error
+
+		if n, err = w.WriteBit(byte(i), i); err != nil {
+			t.Fatalf("WriteBit happen error %v", err)
+		}
+
+		if n != i {
+			t.Fatalf("WriteBit write size %d, want %d", n, i)
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatalf("WriteBit happen error %v", err)
+	}
+
+	if reflect.DeepEqual(b.Bytes(), exp) == false {
+		t.Fatalf("WriteBit write data %x, want %x", b.Bytes(), exp)
+	}
+}
+
+func TestBitWriteBuffer_WriteBits(t *testing.T) {
+	var tests = []struct {
+		data []byte
+		bits int
+		exp  []byte
+	}{
+		{[]byte{0xff, 0xff}, 1, []byte{0x80}},
+		{[]byte{0xff, 0xff}, 7, []byte{0xfe}},
+		{[]byte{0xff, 0xff}, 8, []byte{0xff}},
+		{[]byte{0xff, 0xff}, 9, []byte{0xff, 0x80}},
+		{[]byte{0xff, 0xff}, 15, []byte{0xff, 0xfe}},
+		{[]byte{0xff, 0xff}, 16, []byte{0xff, 0xff}},
+	}
+
+	for _, tt := range tests {
+		var n int
+		var err error
+
+		b := bytes.NewBuffer([]byte{})
+		w := NewBitWriteBuffer(b)
+
+		if n, err = w.WriteBits(tt.data, tt.bits); err != nil {
+			t.Fatalf("WriteBits happen error %v", err)
+		}
+
+		if err = w.Flush(); err != nil {
+			t.Fatalf("WriteBit happen error %v", err)
+		}
+
+		if n != tt.bits {
+			t.Fatalf("WriteBits write size %d, want %d", n, tt.bits)
+		}
+
+		if reflect.DeepEqual(b.Bytes(), tt.exp) == false {
+			t.Fatalf("WriteBits write data %x, want %x", b.Bytes(), tt.exp)
+		}
+	}
+}
+
+func TestBitWriteBuffer_WriteBits_Loop(t *testing.T) {
+	str := "" +
+		"1" +
+		"10" +
+		"011" +
+		"0100" +
+		"00101" +
+		"000110" +
+		"0000111" +
+		"00001000" +
+		"000001001" +
+		"0000001010"
+	exp := binaryToByteArray(str)
+
+	b := bytes.NewBuffer([]byte{})
+	w := NewBitWriteBuffer(b)
+	for i := 1; i <= 10; i++ {
+		var n int
+		var err error
+
+		data := []byte{0x00, 0x00}
+		if i <= 8 {
+			data[0] = byte(i)
+		} else {
+			data[1] = byte(i)
+		}
+
+		if n, err = w.WriteBits(data, i); err != nil {
+			t.Fatalf("WriteBits happen error %v", err)
+		}
+
+		if n != i {
+			t.Fatalf("WriteBits write size %d, want %d", n, i)
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatalf("WriteBit happen error %v", err)
+	}
+
+	if reflect.DeepEqual(b.Bytes(), exp) == false {
+		t.Fatalf("WriteBits write data %x, want %x", b.Bytes(), exp)
+	}
+}
+
+func TestBitWriteBuffer_Write(t *testing.T) {
+	var tests = []struct {
+		data []byte
+		exp  []byte
+	}{
+		{[]byte{0xfe}, []byte{0xfe}},
+		{[]byte{0xff, 0xfe}, []byte{0xff, 0xfe}},
+	}
+
+	for _, tt := range tests {
+		var n int
+		var err error
+
+		b := bytes.NewBuffer([]byte{})
+		w := NewBitWriteBuffer(b)
+
+		if n, err = w.Write(tt.data); err != nil {
+			t.Fatalf("Write happen error %v", err)
+		}
+
+		if n != len(tt.data)*8 {
+			t.Fatalf("Write write size %d, want %d", n, len(tt.data)*8)
+		}
+
+		if err = w.Flush(); err != nil {
+			t.Fatalf("Write happen error %v", err)
+		}
+
+		if reflect.DeepEqual(b.Bytes(), tt.exp) == false {
+			t.Fatalf("Write write data %x, want %x", b.Bytes(), tt.exp)
+		}
+	}
+}
+
+func TestBitWriteBuffer_Write_Combination(t *testing.T) {
+	str := "" +
+		"01" +
+		"0011_0110" +
+		"111" +
+		"01_0111_0011" +
+		"01_0111_0011" +
+		"0011_0110"
+	exp := binaryToByteArray(str)
+
+	var tests = []struct {
+		bits int
+		data []byte
+	}{
+		{2, []byte{0x01}},
+		{8, []byte{0x36}},
+		{3, []byte{0x07}},
+		{10, []byte{0x01, 0x73}},
+		{10, []byte{0x01, 0x73}},
+		{8, []byte{0x36}},
+	}
+
+	b := bytes.NewBuffer([]byte{})
+	w := NewBitWriteBuffer(b)
+	for _, tt := range tests {
+		var n int
+		var err error
+
+		if tt.bits < 8 {
+			// test WriteBit
+			if n, err = w.WriteBit(tt.data[0], tt.bits); err != nil {
+				t.Fatalf("BitWriteBuffer happen error %v", err)
+			}
+		} else if tt.bits == 8 {
+			// test Write
+			if n, err = w.Write(tt.data); err != nil {
+				t.Fatalf("BitWriteBuffer happen error %v", err)
+			}
+		} else {
+			// test WriteBits
+			if n, err = w.WriteBits(tt.data, tt.bits); err != nil {
+				t.Fatalf("BitWriteBuffer happen error %v", err)
+			}
+		}
+
+		if n != tt.bits {
+			t.Fatalf("BitWriteBuffer write size %d, want %d", n, tt.bits)
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Fatalf("Write happen error %v", err)
+	}
+
+	if reflect.DeepEqual(b.Bytes(), exp) == false {
+		t.Fatalf("BitWriteBuffer write data %x, want %x", b.Bytes(), exp)
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 func binaryToByteArray(str string) []byte {
 	str = strings.Replace(str, "_", "", -1)
