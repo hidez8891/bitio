f6d0991b1c4c5286a9b138deba0f74af1514b5ee
hidez8891 <427357+hidez8891@users.noreply.github.com>
2024-09-30 23:38:21 +0900

    add integer read/write function

---
+++ bitio.go
@@ -0,0 +1,106 @@
+package bitio
+
+import (
+	"encoding/binary"
+	"fmt"
+	"unsafe"
+
+	"golang.org/x/exp/constraints"
+)
+
+type ByteOrder bool
+
+const (
+	BigEndian    ByteOrder = false
+	LittleEndian ByteOrder = true
+)
+
+func Read[T constraints.Integer](br BitReader, nBit int, order ByteOrder, dst *T) error {
+	tsize := int(unsafe.Sizeof(*dst))
+	if tsize*8 < nBit {
+		return fmt.Errorf("read size %d bit exceeds %T type size %d bit", nBit, *dst, tsize*8)
+	}
+	if tsize > 8 {
+		return fmt.Errorf("unsupport %T type", *dst)
+	}
+
+	buf := make([]byte, 8)
+	if n, err := br.ReadBits(buf, nBit); err != nil {
+		return err
+	} else if n != nBit {
+		return fmt.Errorf("insufficient size of read, want %d bit, read %d bit", nBit, n)
+	}
+
+	var value uint64
+	if order == LittleEndian {
+		// little endian
+		// 12bit: 0x123 = 0x*****231 -> 0x****2301 -> 0x2301****
+		if nBit%8 > 0 {
+			leftShift(buf, uint(8-nBit%8))
+			buf[len(buf)-1] >>= uint(8 - nBit%8)
+		}
+		leftShift(buf, uint(8*(8-(nBit+7)/8)))
+		value = binary.LittleEndian.Uint64(buf)
+	} else {
+		// big endian (no shift)
+		// 12bit: 0x123 = 0x*****123
+		value = binary.BigEndian.Uint64(buf)
+	}
+
+	*dst = T(value)
+	return nil
+}
+
+func ReadSlice[T constraints.Integer](br BitReader, elemBit int, order ByteOrder, dst []T) error {
+	length := len(dst)
+	for i := 0; i < length; i++ {
+		if err := Read(br, elemBit, order, &dst[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Write[T constraints.Integer](bw BitWriter, nBit int, order ByteOrder, src T) error {
+	tsize := int(unsafe.Sizeof(src))
+	if tsize*8 < nBit {
+		return fmt.Errorf("write size %d bit exceeds %T type size %d bit", nBit, src, tsize*8)
+	}
+	if tsize > 8 {
+		return fmt.Errorf("unsupport %T type", src)
+	}
+
+	buf := make([]byte, 8)
+	if order == LittleEndian {
+		// little endian
+		// 12bit: 0x123 = 0x2301**** -> 0x****2301 -> 0x*****231
+		binary.LittleEndian.PutUint64(buf, uint64(src))
+		rightShift(buf, 8*uint(8-(nBit+7)/8))
+		if nBit%8 > 0 {
+			buf[len(buf)-1] <<= uint(8 - nBit%8)
+			rightShift(buf, uint(8-nBit%8))
+		}
+	} else {
+		// big endian (no shift)
+		// 12bit: 0x123 = 0x****0123
+		binary.BigEndian.PutUint64(buf, uint64(src))
+	}
+
+	if n, err := bw.WriteBits(buf, nBit); err != nil {
+		return err
+	} else if n != nBit {
+		return fmt.Errorf("insufficient size of write, want %d bit, write %d bit", nBit, n)
+	}
+
+	return nil
+}
+
+func WriteSlice[T constraints.Integer](bw BitWriter, elemBit int, order ByteOrder, src []T) error {
+	length := len(src)
+	for i := 0; i < length; i++ {
+		if err := Write(bw, elemBit, order, src[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hidez8891/bitio
 
 go 1.23.1
+
+require golang.org/x/exp v0.0.0-20240909161429-701f63a606c0
